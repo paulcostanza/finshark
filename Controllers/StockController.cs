83a37d65960ad3b4b749e0b3e42b7cfa351be233
Paul Costanza <paulvincentcostanza@gmail.com>
2026-03-04 10:17:00 -0500

    refactored repository to implement interface

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -32,7 +32,7 @@ namespace finshark.Controllers
         [HttpGet("{id}")] // model binding - we want to bind the id from the route to our parameter
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
-            var stock = await _context.Stocks.FindAsync(id);
+            var stock = await _stockRepo.GetByIdAsync(id);
 
             if (stock == null)
             {
@@ -46,8 +46,7 @@ namespace finshark.Controllers
         public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
         {
             var stockModel = stockDto.ToStockFromCreateDto();
-            await _context.Stocks.AddAsync(stockModel);
-            await _context.SaveChangesAsync();
+            await _stockRepo.CreateAsync(stockModel);
 
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
         }
@@ -56,21 +55,12 @@ namespace finshark.Controllers
         [Route("{id}")]
         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
         {
-            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            var stockModel = await _stockRepo.UpdateAsync(id, updateDto);
 
             if (stockModel == null)
             {
-                return NotFound();
+               return NotFound();
             }
-
-            stockModel.Symbol = updateDto.Symbol;
-            stockModel.CompanyName = updateDto.CompanyName;
-            stockModel.Purchase = updateDto.Purchase;
-            stockModel.LastDiv = updateDto.LastDiv;
-            stockModel.Industry = updateDto.Industry;
-            stockModel.MarketCap = updateDto.MarketCap;
-
-            await _context.SaveChangesAsync();
 
             return Ok(stockModel.ToStockDto());
         }
@@ -79,15 +69,12 @@ namespace finshark.Controllers
         [Route("{id}")]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
-            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            var stockModel = await _stockRepo.DeleteAsync(id);
 
             if (stockModel == null)
             {
                 return NotFound();
             }
-
-            _context.Stocks.Remove(stockModel); // remove is NOT an async method, it just marks the entity for deletion, the actual deletion happens when we call SaveChanges
-            await _context.SaveChangesAsync();
 
             return NoContent();
         }
--- Interfaces/IStockRepository.cs
+++ Interfaces/IStockRepository.cs
@@ -1,4 +1,5 @@
-﻿using finshark.Models;
+﻿using finshark.Dtos.Stock;
+using finshark.Models;
 
 namespace finshark.Interfaces
 {
@@ -7,5 +8,9 @@ namespace finshark.Interfaces
         // interfaces allow us to plug in this code into other places,
         // which allow us to abstract our code away
         Task<List<Stock>> GetAllAsync();
+        Task<Stock?> GetByIdAsync(int id); // FirstOrDefault can be NULL
+        Task<Stock> CreateAsync(Stock stockModel);
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
+        Task<Stock?> DeleteAsync(int id);
     }
 }
