24158251493a879b2f3a4d413b17fabb548ebf8f
Paul Costanza <paulvincentcostanza@gmail.com>
2026-03-05 11:44:03 -0500

    added paggination to stocks > getAll

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -59,7 +59,9 @@ namespace finshark.Repository
                 }
             }
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
