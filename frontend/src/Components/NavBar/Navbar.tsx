80f1a883f7744f2e2aea9c4871a126b36d530bb6
Paul Costanza <paulvincentcostanza@gmail.com>
2026-03-12 16:40:24 -0400

    added logout

--- frontend/src/Components/NavBar/Navbar.tsx
+++ frontend/src/Components/NavBar/Navbar.tsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom'
 import Logo from './logo.png'
+import { useAuth } from '../../Context/useAuth';
 
 interface Props { }
 
 const Navbar = (props: Props) => {
+    const { isLoggedIn, user, logout } = useAuth();
+
     return (
         <nav className="relative container mx-auto p-6">
             <div className="flex items-center justify-between">
@@ -21,39 +24,31 @@ const Navbar = (props: Props) => {
 
                 </div>
                 <div className=" lg:flex items-center space-x-6 text-back">
-                    <Link to="/login" className="hover:text-darkBlue">
-                        Login
-                    </Link>
-                    <Link
-                        to="/register"
-                        className="px-8 py-3 font-bold rounded text-black bg-lightGreen hover:opacity-70"
-                    >
-                        Signup
-                    </Link>
+                    {isLoggedIn() ? (
+                        <div className="lg:flex items-center space-x-6 text-back">
+                            <div className="hover:text-darkBlue">Welcome, {user?.userName}</div>
+                            <a
+                                onClick={logout}
+                                className="px-8 py-3 font-bold rounded text-black bg-lightGreen hover:opacity-70"
+                            >
+                                Logout
+                            </a>
+                        </div>
+                    ) : (
+                        <div className="lg:flex items-center space-x-6 text-back">
+                            <Link to="/login" className="hover:text-darkBlue">
+                                Login
+                            </Link>
+                            <Link
+                                to="/register"
+                                className="px-8 py-3 font-bold rounded text-black bg-lightGreen hover:opacity-70"
+                            >
+                                Signup
+                            </Link>
+                        </div>
+                    )}
                 </div>
-                {/* {isLoggedIn() ? (
-                    <div className="hidden lg:flex items-center space-x-6 text-back">
-                        <div className="hover:text-darkBlue">Welcome, {user?.userName}</div>
-                        <a
-                            onClick={logout}
-                            className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
-                        >
-                            Logout
-                        </a>
-                    </div>
-                ) : (
-                    <div className="hidden lg:flex items-center space-x-6 text-back">
-                        <Link to="/login" className="hover:text-darkBlue">
-                            Login
-                        </Link>
-                        <Link
-                            to="/register"
-                            className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
-                        >
-                            Signup
-                        </Link>
-                    </div>
-                )} */}
+
             </div>
         </nav>
     )
