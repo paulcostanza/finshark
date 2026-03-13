d3d26e4a41c77dfc6cbc2fc38e0b00132aaea94b
Paul Costanza <paulvincentcostanza@gmail.com>
2026-03-13 09:00:41 -0400

    added comment box UI, gets added to db

---
+++ frontend/src/Services/CommentService.tsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+import type { CommentPost } from "../Models/Comment";
+import { handleError } from "../Helpers/ErrorHandler";
+
+const api = "http://localhost:5166/api/comment/";
+
+export const commentPostAPI = async (title: string, content: string, symbol: string) => {
+    try {
+        const data = await axios.post<CommentPost>(api + `${symbol}`, {
+            title: title,
+            content: content,
+        })
+
+        return data;
+    } catch (error: any) {
+        handleError(error);
+    }
+}
