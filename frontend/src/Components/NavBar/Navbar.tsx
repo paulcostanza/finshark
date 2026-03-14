7e46566f8ae31db795951fed119ae4cd5d84d740
Paul Costanza <paulvincentcostanza@gmail.com>
2026-03-14 10:46:34 -0400

    edit layout for mobile

--- frontend/src/Components/NavBar/Navbar.tsx
+++ frontend/src/Components/NavBar/Navbar.tsx
@@ -8,41 +8,38 @@ const Navbar = (props: Props) => {
     const { isLoggedIn, user, logout } = useAuth();
 
     return (
-        <nav className="relative container mx-auto p-6">
-            <div className="flex items-center justify-between">
-                <div className="flex items-center space-x-20">
+        <nav className="relative container mx-auto mt-10 p-1">
+            <div className="flex flex-col items-center justify-between">
+                <div className="flex flex-col items-center ">
                     <Link to="/">
                         <img src={Logo} alt='logo header for website of Finny showing off the goods' style={{ maxWidth: '300px' }} />
                     </Link>
-                    <div><h2>Swimming in that money</h2></div>
-                    <div className=" font-bold lg:flex">
-                        <Link to="/search" className="text-black hover:text-darkBlue">
-                            Search
-                        </Link>
+                    <div>
+                        <h2>Swimming in that money</h2>
                     </div>
-
-
                 </div>
-                <div className=" lg:flex items-center space-x-6 text-back">
+
+                <div className="flex justify-center items-center gap-4">
                     {isLoggedIn() ? (
-                        <div className="lg:flex items-center space-x-6 text-back">
-                            <div className="hover:text-darkBlue">Welcome, {user?.userName}</div>
-                            <a
-                                onClick={logout}
-                                className="px-8 py-3 font-bold rounded text-black bg-lightGreen hover:opacity-70"
-                            >
-                                Logout
-                            </a>
+                        <div className="flex flex-col items-center gap-4">
+                            <div>Welcome, {user?.userName}!</div>
+
+                            <div className='flex gap-4'>
+                                <a onClick={logout} className='bg-[#0b5fa5] text-white px-4 py-2 rounded'>
+                                    Logout
+                                </a>
+
+                                <Link to="/search" className='bg-[#39b54a] text-white px-4 py-2 rounded'>
+                                    Search
+                                </Link>
+                            </div>
                         </div>
                     ) : (
-                        <div className="lg:flex items-center space-x-6 text-back">
-                            <Link to="/login" className="hover:text-darkBlue">
+                        <div className="flex items-center gap-4">
+                            <Link to="/login">
                                 Login
                             </Link>
-                            <Link
-                                to="/register"
-                                className="px-8 py-3 font-bold rounded text-black bg-lightGreen hover:opacity-70"
-                            >
+                            <Link to="/register">
                                 Signup
                             </Link>
                         </div>
